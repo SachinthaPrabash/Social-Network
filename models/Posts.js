9db002f0245cf9dd33ade2fd6057ae589623b0e0
sachintha prabash <57107463+SachinthaPrabash@users.noreply.github.com>
2023-03-09 00:17:16 +0530

    posts api done

---
+++ models/Posts.js
@@ -0,0 +1,55 @@
+import mongoose, { Schema } from "mongoose";
+
+const PostSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            data: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    data: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export default mongoose.model('post', PostSchema);
+
